073a0e886f269e18ceadc8927e9ce99dc70f4048
smil187 <98243256+smil187@users.noreply.github.com>
2022-04-25 16:42:24 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -9,4 +9,8 @@ var radarDisplayOptions = {
 };
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
-$.getJSON(weatherAlertsUrl, function(data) {L.geoJSON(data).addTo(map);});
+$.getJSON(weatherAlertsUrl, function(data) {L.geoJSON(data, {
+  style: { color: 'orange' },
+  // more options go here
+}).addTo(map);
+;});
